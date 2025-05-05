d49a6a0050dcee9c8aeed7f062f6abb8e3dd6349
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2025-05-06 00:57:04 +0300

    Slow endpoint for DDoS

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from database import init_db
 from routers.sql_injection import router as sql_router
 from routers.xss import router as xss_router
 from routers.ssrf import router as ssrf_router
-# from routers.ddos import router as ddos_router
+from routers.ddos import router as ddos_router
 from routers.xxe import router as xxe
 
 @asynccontextmanager
@@ -23,7 +23,7 @@ app = FastAPI(title="SSD our api",
 app.include_router(sql_router)
 app.include_router(xss_router)
 app.include_router(ssrf_router)
-# app.include_router(ddos_router)
+app.include_router(ddos_router)
 app.include_router(xxe)
 
 if __name__ == "__main__":
